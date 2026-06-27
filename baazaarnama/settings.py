f55eade3ea90beca8b7dda2bc520a236bc940c85
Vani Singh <vs8648711@gmail.com>
2026-06-27 20:31:47 +0530

    Configure WhiteNoise for static files

--- baazaarnama/settings.py
+++ baazaarnama/settings.py
@@ -71,6 +71,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -150,6 +151,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 #MEDIA FILES
 MEDIA_URL = '/media/'
